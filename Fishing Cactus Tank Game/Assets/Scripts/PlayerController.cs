095d5b5c490936e75ba09acd718af818b7f1556e
corentinbslmc <57088732+corentinbslmc@users.noreply.github.com>
2019-10-28 11:19:17 +0100

    Update PlayerController.cs
    Set  mouvement of player

--- Fishing Cactus Tank Game/Assets/Scripts/PlayerController.cs
+++ Fishing Cactus Tank Game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Rigidbody PlayerRb;
+    public float speed = 50;
+    public float turnSpeed = 30;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+        PlayerRb.transform.Translate(Vector3.forward * Time.deltaTime * speed * VerticalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
